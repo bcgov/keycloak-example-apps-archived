b94f163cc2c659d90631beaa421c292cad6dbb61
jlangy <37274633+jlangy@users.noreply.github.com>
2021-09-27 20:45:53 +0000

    Deploying to gh-pages from @ bcgov/keycloak-example-apps@c6932135b8fb22f5272ba70e9a81618636422264 🚀

---
+++ _next/static/OGh9xj5vqU-7zj4bHdCjK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/754-86e81821db9e438a29f2.js","static/chunks/pages/index-a8c7ce64b42c38faf960.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
