9bd795330d69f054aadbb31fc3973bfb079422e2
Jonathan Langlois <jon@button.is>
2021-09-27 16:50:29 -0700

    feat: client form
    allow users to enter their own public client

--- demo-spa-only/components/KeycloakTable.js
+++ demo-spa-only/components/KeycloakTable.js
@@ -1,25 +1,81 @@
 import { Table } from "semantic-ui-react";
+import { Menu, Segment } from "semantic-ui-react";
+import { useState } from "react";
 
-export default function KeycloakTable({keycloak }) {
-  return (
-    <>
-      {keycloak.authenticated && (
+const Contents = ({ keycloak, activeItem }) => {
+  const input = keycloak[activeItem];
+
+  if (typeof input === "string")
+    return <Segment className="overflow-wrap">{input}</Segment>;
+  if (typeof input === "object")
+    return (
+      <>
         <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell colSpan='2'>ID Token Details</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
           <Table.Body>
-            {keycloak.idTokenParsed &&
-              Object.entries(keycloak.idTokenParsed).map(([key, val]) => (
-                <Table.Row>
-                  <Table.Cell>{key}</Table.Cell>
-                  <Table.Cell>{val}</Table.Cell>
-                </Table.Row>
-              ))}
+            {Object.entries(keycloak[activeItem]).map(([key, val]) => (
+              <Table.Row>
+                <Table.Cell>{key}</Table.Cell>
+                <Table.Cell>{val}</Table.Cell>
+              </Table.Row>
+            ))}
           </Table.Body>
         </Table>
+      </>
+    );
+  return null;
+};
+
+export default function KeycloakTable({ keycloak }) {
+  const [activeItem, setActiveItem] = useState("idToken");
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+  return (
+    <>
+      {keycloak?.authenticated && (
+        <>
+          <Menu attached="top" tabular>
+            <Menu.Item
+              name="idToken"
+              active={activeItem === "idToken"}
+              onClick={handleItemClick}
+            >
+              ID Token Raw{" "}
+            </Menu.Item>
+            <Menu.Item
+              name="idTokenParsed"
+              active={activeItem === "idTokenParsed"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="token"
+              active={activeItem === "token"}
+              onClick={handleItemClick}
+            >
+              Token Raw{" "}
+            </Menu.Item>
+            <Menu.Item
+              name="tokenParsed"
+              active={activeItem === "tokenParsed"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="refreshToken"
+              active={activeItem === "refreshToken"}
+              onClick={handleItemClick}
+            >
+              Refresh Token Raw{" "}
+            </Menu.Item>
+            <Menu.Item
+              name="refreshTokenParsed"
+              active={activeItem === "refreshTokenParsed"}
+              onClick={handleItemClick}
+            />
+          </Menu>
+          <Contents
+            keycloak={keycloak}
+            activeItem={activeItem}
+            style={{ maxWidth: "100%" }}
+          />
+        </>
       )}
     </>
   );
--- demo-spa-only/pages/_app.js
+++ demo-spa-only/pages/_app.js
@@ -1,8 +1,62 @@
 import '../styles/globals.css';
 import 'semantic-ui-css/semantic.min.css';
+import { useEffect, useState } from "react";
+const BASE_PATH = '/keycloak-example-apps'
+
+const getKeycloakConfig = (customConfig) => {
+  try {
+    if (customConfig) {
+      const {authServer, realm, resource} = customConfig;
+      return {
+        url: authServer,
+        realm,
+        clientId: resource
+      }
+    } else {
+      const storageConfig = JSON.parse(window.sessionStorage.getItem('kcConfig'))
+      const {authServer, realm, resource} = storageConfig;
+      return {
+        url: authServer,
+        realm,
+        clientId: resource
+      }
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [keycloak, setKeycloak] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [customConfig, setCustomConfig] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    let Keycloak = require("keycloak-js");
+    let keycloak;
+    if (customConfig || window.sessionStorage.getItem('kcConfig')) {
+      const config = getKeycloakConfig(customConfig)
+      keycloak = new Keycloak(config);
+    } else {
+      keycloak = Keycloak(`${BASE_PATH}/keycloak.json`);
+    }
+    const initKeycloak = async () => {
+      if (window && typeof window !== "object") return;
+      keycloak
+        .init({
+          pkceMethod: "S256",
+          redirectUri: "https://bcgov.github.io/keycloak-example-apps/",
+          idpHint: "idir",
+        })
+        .then(() => {
+          setKeycloak(keycloak);
+          setLoading(false);
+        });
+    };
+    initKeycloak();
+  }, [customConfig]);
+  return <Component {...pageProps} loading={loading} keycloak={keycloak} setCustomConfig={setCustomConfig}/>;
 }
 
 export default MyApp;
