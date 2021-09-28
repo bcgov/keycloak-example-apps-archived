d72205eb41730f4052e8710cbc88c9bf568ee28c
Jonathan Langlois <jon@button.is>
2021-09-28 11:28:47 -0700

    chore: change config
    give user the option to change their adapter config

--- demo-spa-only/pages/index.js
+++ demo-spa-only/pages/index.js
@@ -2,22 +2,39 @@ import { Container, Button, Message } from "semantic-ui-react";
 import KeycloakTable from "components/KeycloakTable";
 import ClientForm from "components/ClientForm";
 
-export default function Home({loading, keycloak, setCustomConfig}) {
-  const handleLogin = () => keycloak?.login({ idpHint: "idir" });
+export default function Home({
+  loading,
+  keycloak,
+  setCustomConfig,
+  adapterConfig,
+  setAdapterConfig,
+}) {
+  const handleLogin = () => {
+    keycloak?.login(adapterConfig);
+  };
   const handleLogout = () => keycloak?.logout();
 
   return (
     <Container>
-      <br/>
+      <br />
       <h1>Keycloak Playground</h1>
-      <p>This is a playground application for using the <code>keycloak-js</code> adapter. Click the <strong>Login</strong> button
-      below to login with the default client (uses IDIR as an IDP) and see your different token details. If you have your own
-      public client, you can use the form below (Click on <strong>Set My Own Client</strong>) to authenticate to your own client.</p>
+      <p>
+        This is a playground application for using the <code>keycloak-js</code>{" "}
+        adapter. Click the <strong>Login</strong> button below to login with the
+        default client (uses IDIR as an IDP) and see your different token
+        details. If you have your own public client, you can use the form below
+        (Click on <strong>Set My Own Client</strong>) to authenticate to your
+        own client.
+      </p>
       <Button onClick={handleLogin}>Login</Button>
       <Button onClick={handleLogout}>Logout</Button>
       <br />
       <br />
-      <ClientForm setCustomConfig={setCustomConfig} />
+      <ClientForm
+        setCustomConfig={setCustomConfig}
+        adapterConfig={adapterConfig}
+        setAdapterConfig={setAdapterConfig}
+      />
       <br />
       {loading ||
         (!keycloak?.authenticated && (
