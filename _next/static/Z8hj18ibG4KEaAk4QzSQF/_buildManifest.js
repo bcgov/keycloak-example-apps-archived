111500dd5312f22e0d8ab39bd955ebf969ece0af
junminahn <36021827+junminahn@users.noreply.github.com>
2022-04-04 21:11:38 +0000

    Deploying to gh-pages from @ bcgov/keycloak-example-apps@f9fd2f0f0568066fe1c7debf3b612375bba4b760 🚀

---
+++ _next/static/Z8hj18ibG4KEaAk4QzSQF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-a2d0c8e72019a3ed.js","static/chunks/638-b95e464065546b86.js","static/chunks/pages/index-283c23b43b14daf8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
