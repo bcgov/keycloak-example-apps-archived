0c5434b82329012ce0d0432b9f5bfbfb9eaed5ac
ZHE CHEN <kzsyixi@gmail.com>
2022-08-23 14:24:56 -0700

    feat: update public-spa app with .env, README files

--- public-examples/public-spa/next.config.js
+++ public-examples/public-spa/next.config.js
@@ -3,6 +3,15 @@ const BASE_PATH = process.env.APP_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: true,
   basePath: BASE_PATH,
+  publicRuntimeConfig: {
+    HOSTNAME: process.env.HOST || '0.0.0.0',
+    PORT: process.env.PORT || 3000,
+    OIDC_CLIENT_ID: process.env.OIDC_CLIENT_ID || '',
+    OIDC_AUTHORIZATION_URL: `${process.env.CSS_DOMAIN_NAME_URL}` || '',
+    OIDC_REALM_TYPE: `${process.env.OIDC_REALM_TYPE}` || '',
+    OIDC_REDIRECT_URL: process.env.OIDC_REDIRECT_URL || '',
+    OIDC_LOGOUT_REDIRECT_URL: process.env.OIDC_LOGOUT_REDIRECT_URL || '',
+  },
 };
 
 module.exports = nextConfig;
