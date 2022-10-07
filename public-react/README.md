da76952cdc2700fb460c1a5e7d8af923378dd1a2
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-10-07 08:49:38 -0700

    chore: addressed pr comments

--- public-react/README.md
+++ public-react/README.md
@@ -16,9 +16,10 @@ This is an example react app that authenticates users using OAuth 2.0 standard f
 - Update below required values and add them to a `.env` file and save it in project directory
 
   ```sh
-  REACT_APP_URL=http://localhost:3000
-  REACT_APP_REALM=standard
-  REACT_APP_CLIENT_ID=<UPDATE>
+  REACT_APP_SSO_REDIRECT_URI=http://localhost:3000
+  REACT_APP_SSO_AUTH_SERVER_URL=https://dev.loginproxy.gov.bc.ca/auth
+  REACT_APP_SSO_REALM=standard
+  REACT_APP_SSO_CLIENT_ID=<UPDATE>
   ```
 
 In the project directory, you can run:
--- public-react/src/services/keycloak.js
+++ public-react/src/services/keycloak.js
@@ -1,20 +1,25 @@
 import Keycloak from 'keycloak-js';
 
 const _kc = new Keycloak({
-  url: 'https://dev.loginproxy.gov.bc.ca/auth',
-  realm: process.env.REACT_APP_REALM,
-  clientId: process.env.REACT_APP_CLIENT_ID,
+  url: process.env.REACT_APP_SSO_AUTH_SERVER_URL,
+  realm: process.env.REACT_APP_SSO_REALM,
+  clientId: process.env.REACT_APP_SSO_CLIENT_ID,
 });
 
 const loginOptions = {
+  // https://logon7.gov.bc.ca/clp-cgi/logoff.cgi uri performs siteminder logout
   redirectUri: `https://logon7.gov.bc.ca/clp-cgi/logoff.cgi?retnow=1&returl=${encodeURIComponent(
-    process.env.REACT_APP_URL,
+    process.env.REACT_APP_SSO_REDIRECT_URI,
   )}`,
   idpHint: '',
 };
 
 export const initializeKeycloak = async () => {
   try {
+    _kc.onTokenExpired = () => {
+      _kc.updateToken();
+    };
+
     const auth = await _kc.init({
       pkceMethod: 'S256',
       checkLoginIframe: false,
